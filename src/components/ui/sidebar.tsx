e9ef09830fe27c4c11f8401d0e64b762c5b58b60
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2025-01-30 08:08:49 -0300

    minor adjustment

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,7 +2,8 @@
 
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import type { VariantProps, cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 import { PanelLeft } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
